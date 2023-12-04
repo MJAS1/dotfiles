c968a05e95a2acd5fbbcde8e8002fef2aef799d1
Manu Sutela <manu.sutela@gmail.com>
2023-12-04 15:48:26 +0200

    Auto resize windows

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -41,4 +41,16 @@ vim.api.nvim_command("autocmd TermOpen * setlocal norelativenumber")
 
 vim.opt.termguicolors = true
 
+local wr_group = vim.api.nvim_create_augroup('WinResize', { clear = true })
+
+vim.api.nvim_create_autocmd(
+    'VimResized',
+    {
+        group = wr_group,
+        pattern = '*',
+        command = 'wincmd =',
+        desc = 'Automatically resize windows when the host window size changes.'
+    }
+)
+
 require('manu.lazy')
