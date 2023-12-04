499e89c0b13bbcddf9be49684af8b91a8e2bfa99
Manu Sutela <manu.sutela@gmail.com>
2023-12-04 15:40:13 +0200

    Add overseer plugin

---
+++ config/nvim/lua/manu/plugins/overseer.lua
@@ -0,0 +1,22 @@
+return {
+    'stevearc/overseer.nvim',
+    config = function()
+        require('overseer').setup({
+            strategy = {
+                'toggleterm'
+            },
+            default_vscode = {
+                default = {
+                    "on_output_summarize",
+                    "on_exit_set_status",
+                    "on_complete_notify",
+                    "on_complete_dispose",
+                    { "on_result_diagnostics_quickfix", open = true },
+                    "on_result_diagnostics",
+                },
+            },
+
+        })
+        vim.keymap.set('', '<C-b>', '<cmd>OverseerRun<CR>', { remap = true })
+    end
+}
