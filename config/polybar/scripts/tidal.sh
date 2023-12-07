a95b2ca6b0d0ec71ec7013b144dc76d7cd25ec04
Manu Sutela <manu.sutela@gmail.com>
2023-12-07 15:54:44 +0200

    Add tidal support to polybar

---
+++ config/polybar/scripts/tidal.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Adjusted from: https://github.com/Mastermindzh/dotfiles/tree/master
+
+TIDAL_CLI=~/bin/tidal-cli.sh
+case $BLOCK_BUTTON in
+1) "$TIDAL_CLI" playpause ;; # left click
+4) "$TIDAL_CLI" next ;; # scroll up
+5) "$TIDAL_CLI" previous ;; # scroll down
+esac
+
+if "$TIDAL_CLI" status | grep 'paused' >/dev/null; then
+  printf '  ' # fa-pause
+else
+  printf '  ' # fa-play
+fi
+"$TIDAL_CLI" info
