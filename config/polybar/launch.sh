f05e65b4cf0a8870a415bec08c89141eacc3418b
Manu Sutela <manu.sutela@gmail.com>
2023-12-12 11:42:35 +0200

    Implement polybar launching for different screen configurations

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -2,4 +2,11 @@
 
 polybar-msg cmd quit
 
-polybar
+if autorandr --current | grep --quiet office; then
+    MONITOR=DP-3-1 polybar office
+    MONITOR=DP-3-3 polybar office
+elif autorandr --current | grep --quiet remote; then
+    polybar remote
+else
+    polybar main
+fi
