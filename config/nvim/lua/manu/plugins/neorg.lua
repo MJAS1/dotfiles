4a16e80ef91aecbb5f416259467e6e5c793f5131
Manu Sutela <manu.sutela@gmail.com>
2024-06-18 18:10:40 +0300

    Update neorg

--- config/nvim/lua/manu/plugins/neorg.lua
+++ config/nvim/lua/manu/plugins/neorg.lua
@@ -1,22 +1,17 @@
 return {
     {
-        'nvim-neorg/neorg',
-        build = ':Neorg sync-parsers',
-        dependencies = { 'nvim-lua/plenary.nvim' },
+        "vhyrro/luarocks.nvim",
+        priority = 1000, -- We'd like this plugin to load first out of the rest
+        config = true, -- This automatically runs `require("luarocks-nvim").setup()`
+    },
+    {
+        "nvim-neorg/neorg",
+        dependencies = { "luarocks.nvim" },
+        -- put any other flags you wanted to pass to lazy here!
         config = function()
-            require('neorg').setup {
-                load = {
-                    ['core.defaults'] = {},
-                    ['core.concealer'] = {},
-                    ['core.dirman'] = {
-                        config = {
-                            workspaces = {
-                                notes = '~/notes',
-                            },
-                        },
-                    },
-                },
-            }
+            require("neorg").setup({
+                notes = '~/notes',
+            })
         end,
     },
 }
