eabc5419d37958c47609b65541bb36f7939e3d30
Manu Sutela <manu.sutela@gmail.com>
2024-08-31 23:41:39 +0300

    Toggle diagnostics (neovim 0.10.1 required)

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -123,4 +123,14 @@ vim.diagnostic.config({
     },
 })
 
+-- Toggle diagnostics
+vim.keymap.set('n', '<leader>td', function()
+    vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+    end,
+    {
+        silent = true,
+        noremap = true
+    }
+)
+
 require('manu.lazy')
