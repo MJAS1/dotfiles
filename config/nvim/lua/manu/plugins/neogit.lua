3560b962fa6a86e1008013aaba91765dda11fc1c
Manu Sutela <manu.sutela@gmail.com>
2024-09-12 15:07:31 +0300

    Add dependencies for neogit

--- config/nvim/lua/manu/plugins/neogit.lua
+++ config/nvim/lua/manu/plugins/neogit.lua
@@ -1,5 +1,10 @@
 return {
   'NeogitOrg/neogit',
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
   config = function()
       local neogit = require('neogit')
       neogit.setup()
