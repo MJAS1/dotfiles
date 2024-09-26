7a45bc12b06d360563c501bcca116248ac558ff3
Manu Sutela <manu.sutela@gmail.com>
2024-09-26 12:06:10 +0300

    Accept whole suggestion with <C-e>

--- config/nvim/lua/manu/plugins/copilot.lua
+++ config/nvim/lua/manu/plugins/copilot.lua
@@ -13,7 +13,7 @@ return {
               enabled = true,
               auto_trigger = true,
               keymap = {
-                  accept_line = '<C-e>',
+                  accept = '<C-e>',
                   accept_word = '<M-f>',
               }
           },
