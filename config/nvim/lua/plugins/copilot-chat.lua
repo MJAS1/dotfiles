6fcc756b1afc00b6ecdcf630ba1d5f36fdeda1bf
Manu Sutela <manu.sutela@gmail.com>
2025-04-15 14:04:18 -0400

    Replace conflicting keymap

--- config/nvim/lua/plugins/copilot-chat.lua
+++ config/nvim/lua/plugins/copilot-chat.lua
@@ -14,6 +14,6 @@ return {
             }
         }
     },
-    vim.keymap.set('n', '<leader>l', '<cmd>CopilotChatOpen<CR>', { remap = true })
+    vim.keymap.set('n', '<leader>c', '<cmd>CopilotChatOpen<CR>', { noremap = true })
   },
  }
