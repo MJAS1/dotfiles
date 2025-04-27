ecd3c484da1a2fb73143c3d2e1ccc9ddbce6e174
Manu Sutela <manu.sutela@gmail.com>
2025-04-27 13:22:50 -0400

    Allow jumping between elements

--- config/nvim/lua/plugins/nvim-cmp.lua
+++ config/nvim/lua/plugins/nvim-cmp.lua
@@ -30,6 +30,25 @@ return {
                 ['<C-space>'] = cmp.mapping.complete(),
                 ['<C-c>'] = cmp.mapping.close(),
                 ['<CR>'] = cmp.mapping.confirm({ select = false }),
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.locally_jumpable(1) then
+                        luasnip.jump(1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.locally_jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
             }),
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
