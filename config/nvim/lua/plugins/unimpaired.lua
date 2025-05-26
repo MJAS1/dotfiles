bd945f8987386665f449d1124b67e9c3dc58a7b7
Manu Sutela <manu.sutela@gmail.com>
2025-05-26 16:33:40 +0300

    Add description for unimpaired plugin keymaps

--- config/nvim/lua/plugins/unimpaired.lua
+++ config/nvim/lua/plugins/unimpaired.lua
@@ -1,9 +1,9 @@
 return {
     'tpope/vim-unimpaired',
     config = function()
-        vim.keymap.set('n', '<M-k>', '[e', { remap = true })
-        vim.keymap.set('n', '<M-j>', ']e', { remap = true })
-        vim.keymap.set('v', '<M-k>', '[egv', { remap = true })
-        vim.keymap.set('v', '<M-j>', ']egv', { remap = true })
+        vim.keymap.set('n', '<M-k>', '[e', { remap = true, desc = 'Move current line up' })
+        vim.keymap.set('n', '<M-j>', ']e', { remap = true, desc = 'Move current line down' })
+        vim.keymap.set('v', '<M-k>', '[egv', { remap = true, desc = 'Move selected lines up' })
+        vim.keymap.set('v', '<M-j>', ']egv', { remap = true, desc = 'Move selected lines down' })
     end,
 }
