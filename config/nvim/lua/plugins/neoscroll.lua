85b779abb8ba15569b0ac4d0c42e0b034a8aed80
Manu Sutela <manu.sutela@gmail.com>
2025-06-30 12:35:05 +0300

    Don't map ctrl-f in neoscroll

--- config/nvim/lua/plugins/neoscroll.lua
+++ config/nvim/lua/plugins/neoscroll.lua
@@ -1,6 +1,16 @@
 return {
-  "karb94/neoscroll.nvim",
+  'karb94/neoscroll.nvim',
   opts = {
       duration_multiplier = 0.4,
+        mappings = {
+            '<C-u>',
+            '<C-d>',
+            '<C-b>',
+            '<C-y>',
+            '<C-e>',
+            'zt',
+            'zz',
+            'zb',
+        },
   },
 }
