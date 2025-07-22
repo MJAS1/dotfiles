8b35cdf2a16cb6c3e0f51ed6bb4f49a8e9455734
Manu Sutela <manu.sutela@gmail.com>
2025-07-22 13:49:34 +0300

    Fix indentation

--- config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -1,35 +1,35 @@
 return {
-    'nvim-tree/nvim-tree.lua',
-    version = '*',
-    lazy = false,
-    dependencies = {
-        'nvim-tree/nvim-web-devicons',
-    },
-    config = function()
-        require('nvim-tree').setup({
-            view = {
-                relativenumber = true,
-                adaptive_size = true,
+  'nvim-tree/nvim-tree.lua',
+  version = '*',
+  lazy = false,
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+  config = function()
+    require('nvim-tree').setup({
+      view = {
+        relativenumber = true,
+        adaptive_size = true,
+      },
+      renderer = {
+        indent_markers = {
+          enable = true,
+        },
+        icons = {
+          glyphs = {
+            folder = {
+              arrow_closed = '▸',
+              arrow_open = '▾',
             },
-            renderer = {
-                indent_markers = {
-                    enable = true,
-                },
-                icons = {
-                    glyphs = {
-                        folder = {
-                            arrow_closed = '▸',
-                            arrow_open = '▾',
-                        },
-                    },
-                },
-            },
-            git = {
-                show_on_dirs = false,
-            },
-        })
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
-        vim.keymap.set('', '<C-n>', '<cmd>NvimTreeToggle<CR>')
-    end
+          },
+        },
+      },
+      git = {
+        show_on_dirs = false,
+      },
+    })
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+    vim.keymap.set('', '<C-n>', '<cmd>NvimTreeToggle<CR>')
+  end
 }
