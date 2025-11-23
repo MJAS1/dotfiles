5bea677d6db76e1d3a59eee7114c0b348b1bc88e
Manu Sutela <manu.sutela@gmail.com>
2025-11-23 14:00:37 +0200

    Don't use LSP as fallback and remove commented code

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -3,33 +3,12 @@ return {
   lazy = true,
   event = { 'BufReadPre', 'BufNewFile' }, -- to disable, comment this out
   config = function()
-    -- local conform = require('conform')
-    --
-    -- -- conform.setup({
-    -- --   formatters_by_ft = {
-    -- --     python = { 'isort', 'black' },
-    -- --   },
-    -- --   format_on_save = {
-    -- --     lsp_fallback = true,
-    -- --     async = false,
-    -- --     timeout_ms = 1000,
-    -- --   },
-    -- -- })
-    --
-    -- vim.keymap.set({ 'n', 'v' }, '<leader>mp', function()
-    --   conform.format({
-    --     lsp_fallback = true,
-    --     async = false,
-    --     timeout_ms = 1000,
-    --   })
-    -- end, { desc = 'Format file or range (in visual mode)' })
     local conform = require('conform')
     conform.setup({
       formatters_by_ft = {
         cpp = { 'clang-format' },
       },
       format_on_save = {
-        lsp_fallback = true,
         async = false,
         timeout_ms = 1000,
       },
