06b52538192de0ef732021a8371ef15afbf8f5bf
Manu Sutela <manu.sutela@gmail.com>
2025-12-17 19:27:33 +0200

    Allow project specific nvim config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -12,6 +12,9 @@ vim.opt.autoindent=true
 vim.opt.number=true
 vim.opt.relativenumber=true
 
+vim.opt.exrc = true
+vim.opt.secure = true
+
 vim.opt.autoread=true
 vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
   command = "if mode() != 'c' | checktime | endif",
